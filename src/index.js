db7e7140a2695fa6b6f9aa4ae3f32d36be6f9a66
afeeniphillips <afeeni.phillips@bison.howard.edu>
2020-08-01 09:43:56 -0400

    header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Login from './Views/Login/Login'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = (
+  <Router>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
